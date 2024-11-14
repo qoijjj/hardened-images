f5fb01eaf6638fef918985c21d87dfb4f4f3e61b
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2024-11-14 14:13:52 -0800

    fix

--- files/scripts/enablesecurebluefirstrun.sh
+++ files/scripts/enablesecurebluefirstrun.sh
@@ -3,5 +3,5 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-systemctl enable secureblue-firstrun.service
-systemctl enable secureblue-cleanup.service
+systemctl enable securebluefirstrun.service
+systemctl enable securebluecleanup.service
