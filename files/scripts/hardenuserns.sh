500a6f1a1854e444aee8d15451e7d3989fa0701d
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2024-12-03 14:50:02 -0800

    testing: switch to new selinux policy

---
+++ files/scripts/hardenuserns.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+curl -Lo /etc/yum.repos.d/secureblue-selinux-policy-fedora-41.repo https://copr.fedorainfracloud.org/coprs/secureblue/selinux-policy/repo/fedora-41/secureblue-selinux-policy-fedora-41.repo
+sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/secureblue-selinux-policy-fedora-41.repo
+
+rpm-ostree override replace \
+  --experimental \
+  --from repo='copr:copr.fedorainfracloud.org:secureblue:selinux-policy' \
+    selinux-policy
