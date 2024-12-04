f1d7d9bce668be99e5be7d46d2ec0a20e03382e6
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2024-12-04 12:47:18 -0800

    try something else

--- files/scripts/hardenuserns.sh
+++ files/scripts/hardenuserns.sh
@@ -6,11 +6,5 @@ set -oue pipefail
 curl -Lo /etc/yum.repos.d/secureblue-selinux-policy-fedora-41.repo https://copr.fedorainfracloud.org/coprs/secureblue/selinux-policy/repo/fedora-41/secureblue-selinux-policy-fedora-41.repo
 sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/secureblue-selinux-policy-fedora-41.repo
 
-rpm-ostree override replace \
-  --experimental \
-  --from repo='copr:copr.fedorainfracloud.org:secureblue:selinux-policy' \
-    selinux-policy \
-    selinux-policy-targeted \
-    selinux-policy-devel
-
-semodule -B
+rpm-ostree refresh-md
+rpm-ostree upgrade
