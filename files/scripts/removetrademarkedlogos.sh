417adcf44bd6dc34196d653e4f4c894b275ad45a
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2024-12-18 00:17:09 -0800

    more polish

---
+++ files/scripts/removetrademarkedlogos.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+rm /usr/share/pixmaps/fedora-logo.png
+rm /usr/share/pixmaps/fedora_logo_med.png
+rm /usr/share/pixmaps/fedora-logo-small.png 
+rm /usr/share/pixmaps/fedora-logo-sprite.png
+rm /usr/share/pixmaps/fedora-logo-sprite.svg
+rm /usr/share/pixmaps/fedora_whitelogo.svg
+rm /usr/share/pixmaps/fedora_whitelogo_med.png
+rm /usr/share/pixmaps/system-logo-white.png
